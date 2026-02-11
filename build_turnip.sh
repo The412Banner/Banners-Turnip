60d39d2f7bfcb7e3fb0e98f5353c7a85c449c3ba
StevenMX <alab01845@gmail.com>
2026-02-11 10:03:22 -0300

    Update build_turnip.sh

--- build_turnip.sh
+++ build_turnip.sh
@@ -4,7 +4,7 @@ set -o pipefail
 green='\033[0;32m'
 nocolor='\033[0m'
 
-deps="ninja patchelf unzip curl pip flex bison zip git perl glslangValidator python3 patch"
+deps="ninja patchelf unzip curl pip flex bison zip git perl glslangValidator python3"
 workdir="$(pwd)/turnip_workdir"
 ndkver="android-ndk-r28"
 target_sdk="36" 
@@ -25,105 +25,13 @@ prepare_ndk(){
     export ANDROID_NDK_HOME="$workdir/$ndkver"
 }
 
-inject_mods() {
-    cat << 'EOF_PYTHON' > injector.py
-import re
-import os
-
-phys_dev_file = "src/freedreno/vulkan/tu_physical_device.cc"
-device_file = "src/freedreno/vulkan/tu_device.cc"
-
-with open(device_file, 'r') as f:
-    dev_content = f.read()
-
-dev_content = dev_content.replace('#include "tu_version.h"', '')
-
-pattern_revert = r"char\s+devname\[128\];[\s\S]*?strcat\(devname,[\s\S]*?strcpy\(props->deviceName,\s*devname\);"
-replacement_revert = "strcpy(props->deviceName, pdevice->name);"
-
-if re.search(pattern_revert, dev_content):
-    dev_content = re.sub(pattern_revert, replacement_revert, dev_content)
-
-if 'setenv("WRAPPER_VK_VERSION"' not in dev_content:
-    dev_content = dev_content.replace(
-        "VkResult\ntu_CreateInstance(const VkInstanceCreateInfo *pCreateInfo,",
-        "VkResult\ntu_CreateInstance(const VkInstanceCreateInfo *pCreateInfo,\n   const VkAllocationCallbacks *pAllocator,\n   VkInstance *pInstance)\n{\n   setenv(\"WRAPPER_VK_VERSION\", \"1.4.340\", 1);\n"
-    )
-    dev_content = dev_content.replace(
-        "   const VkAllocationCallbacks *pAllocator,\n   VkInstance *pInstance)\n{\n   setenv(\"WRAPPER_VK_VERSION\", \"1.4.340\", 1);\n\n   const VkAllocationCallbacks *pAllocator,\n   VkInstance *pInstance)",
-        ""
-    )
-
-with open(device_file, 'w') as f:
-    f.write(dev_content)
-
-with open(phys_dev_file, 'r') as f:
-    phys_content = f.read()
-
-phys_content = re.sub(
-    r"return VK_MAKE_VERSION\(1, [0-9]+, [0-9]+\);",
-    "return VK_MAKE_VERSION(1, 4, 340);",
-    phys_content
-)
-
-features_1_1 = [
-    "storageBuffer16BitAccess", "uniformAndStorageBuffer16BitAccess", "storagePushConstant16",
-    "storageInputOutput16", "multiview", "multiviewGeometryShader", "multiviewTessellationShader",
-    "variablePointersStorageBuffer", "variablePointers", "protectedMemory", "samplerYcbcrConversion",
-    "shaderDrawParameters"
-]
-
-features_1_2 = [
-    "samplerMirrorClampToEdge", "drawIndirectCount", "storageBuffer8BitAccess", "uniformAndStorageBuffer8BitAccess",
-    "storagePushConstant8", "shaderBufferInt64Atomics", "shaderSharedInt64Atomics", "shaderFloat16",
-    "shaderInt8", "descriptorIndexing", "shaderInputAttachmentArrayDynamicIndexing",
-    "shaderUniformTexelBufferArrayDynamicIndexing", "shaderStorageTexelBufferArrayDynamicIndexing",
-    "shaderUniformBufferArrayNonUniformIndexing", "shaderSampledImageArrayNonUniformIndexing",
-    "shaderStorageBufferArrayNonUniformIndexing", "shaderStorageImageArrayNonUniformIndexing",
-    "shaderInputAttachmentArrayNonUniformIndexing", "shaderUniformTexelBufferArrayNonUniformIndexing",
-    "shaderStorageTexelBufferArrayNonUniformIndexing", "descriptorBindingUniformBufferUpdateAfterBind",
-    "descriptorBindingSampledImageUpdateAfterBind", "descriptorBindingStorageImageUpdateAfterBind",
-    "descriptorBindingStorageBufferUpdateAfterBind", "descriptorBindingUniformTexelBufferUpdateAfterBind",
-    "descriptorBindingStorageTexelBufferUpdateAfterBind", "descriptorBindingUpdateUnusedWhilePending",
-    "descriptorBindingPartiallyBound", "descriptorBindingVariableDescriptorCount", "runtimeDescriptorArray",
-    "samplerFilterMinmax", "scalarBlockLayout", "imagelessFramebuffer", "uniformBufferStandardLayout",
-    "shaderSubgroupExtendedTypes", "separateDepthStencilLayouts", "hostQueryReset", "timelineSemaphore",
-    "bufferDeviceAddress", "bufferDeviceAddressCaptureReplay", "bufferDeviceAddressMultiDevice",
-    "vulkanMemoryModel", "vulkanMemoryModelDeviceScope", "vulkanMemoryModelAvailabilityVisibilityChains",
-    "shaderOutputViewportIndex", "shaderOutputLayer", "subgroupBroadcastDynamicId"
-]
-
-features_1_3 = [
-    "robustImageAccess", "inlineUniformBlock", "descriptorBindingInlineUniformBlockUpdateAfterBind",
-    "pipelineCreationCacheControl", "privateData", "shaderDemoteToHelperInvocation", "shaderTerminateInvocation",
-    "subgroupSizeControl", "computeFullSubgroups", "synchronization2", "textureCompressionASTC_HDR",
-    "shaderZeroInitializeWorkgroupMemory", "dynamicRendering", "shaderIntegerDotProduct", "maintenance4"
-]
-
-def inject_features(content, struct_name, feat_list, struct_type):
-    code = "".join([f"      f->{feat} = VK_TRUE;\n" for feat in feat_list])
-    pattern = rf"(case {struct_name}:[\s\S]*?)(\s+break;)"
-    replacement = f"\\1\n      {struct_type} *f = ({struct_type} *)ext;\n{code}\\2"
-    return re.sub(pattern, replacement, content, count=1)
-
-phys_content = inject_features(phys_content, "VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_VULKAN_1_1_FEATURES", features_1_1, "VkPhysicalDeviceVulkan11Features")
-phys_content = inject_features(phys_content, "VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_VULKAN_1_2_FEATURES", features_1_2, "VkPhysicalDeviceVulkan12Features")
-phys_content = inject_features(phys_content, "VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_VULKAN_1_3_FEATURES", features_1_3, "VkPhysicalDeviceVulkan13Features")
-
-with open(phys_dev_file, 'w') as f:
-    f.write(phys_content)
-EOF_PYTHON
-    
-    python3 injector.py
-}
-
 compile_mesa() {
-    local repo_url="https://gitlab.freedesktop.org/mesa/mesa.git"
-    local branch="main"
-    local build_name="Turnip-A8xx-Final"
-    local output_tag="V78-A8xx-Final-1.4.340"
+    local repo_url="https://gitlab.freedesktop.org/zdobersek/mesa-fork.git"
+    local branch="work/tu_kgsl_timeline_sync"
+    local build_name="Turnip-Zdobersek-Timeline"
+    local output_tag="V81-Zdobersek-Clean"
 
-    echo -e "${green}Cloning Mesa Main...${nocolor}"
+    echo -e "${green}Cloning Zdobersek Fork...${nocolor}"
     
     cd "$workdir"
     if [ -d mesa ]; then rm -rf mesa; fi
@@ -132,20 +40,8 @@ compile_mesa() {
     cd mesa
     git config user.email "ci@turnip.builder" && git config user.name "Turnip CI Builder"
 
-    if [ -f "$workdir/../tu_gen8.patch" ]; then
-        patch -p1 --fuzz=4 --ignore-whitespace < "$workdir/../tu_gen8.patch" || true
-    elif [ -f "tu_gen8.patch" ]; then
-         patch -p1 --fuzz=4 --ignore-whitespace < "tu_gen8.patch" || true
-    else
-        echo "Error: tu_gen8.patch not found."
-        exit 1
-    fi
-
-    inject_mods
-
-    sed -i 's/VK_MAKE_VERSION(1, 3, [0-9]*)/VK_MAKE_VERSION(1, 4, 340)/g' src/freedreno/vulkan/tu_device.cc || true
-    sed -i 's/VK_MAKE_VERSION(1, 4, [0-9]*)/VK_MAKE_VERSION(1, 4, 340)/g' src/freedreno/vulkan/tu_device.cc || true
-
+    echo -e "${green}Building: $build_name${nocolor}"
+    
     mkdir -p subprojects && cd subprojects
     rm -rf spirv-tools spirv-headers
     git clone --depth=1 https://github.com/KhronosGroup/SPIRV-Tools.git spirv-tools
@@ -211,7 +107,7 @@ EOF
     echo "{
   \"schemaVersion\": 1,
   \"name\": \"$build_name\",
-  \"description\": \"Mesa Main + A8xx + Env Var 1.4.340 + All Feats\",
+  \"description\": \"Clean build from zdobersek/mesa-fork (tu_kgsl_timeline_sync)\",
   \"author\": \"StevenMX\",
   \"packageVersion\": \"1\",
   \"vendor\": \"Mesa\",
